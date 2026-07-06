1ea96cf0efcf7bd00e0efd320e51cbe9a0ebb392
omertt123 <talatomer87@gmail.com>
2026-07-06 23:33:02 +0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -38,3 +38,20 @@ function loadClient() {
     <p>Lots Completed: ${c.lots}</p>
   `;
 }
+function getTarget(acc, dep) {
+  if (acc === "STANDARD") {
+    if (dep >= 20000) return { target: 20000, lots: 50, reward: "$1200" };
+    if (dep >= 15000) return { target: 15000, lots: 40, reward: "$1000" };
+    if (dep >= 10000) return { target: 10000, lots: 25, reward: "$700" };
+    if (dep >= 5000) return { target: 5000, lots: 15, reward: "$300" };
+    if (dep >= 2000) return { target: 2000, lots: 5, reward: "$100" };
+  }
+
+  if (acc === "RAW") {
+    if (dep >= 1750) return { target: 1750, lots: 300, reward: "$18 / Lot" };
+    if (dep >= 1000) return { target: 1000, lots: 200, reward: "$18 / Lot" };
+    if (dep >= 500) return { target: 500, lots: 100, reward: "$18 / Lot" };
+  }
+
+  return null;
+}
